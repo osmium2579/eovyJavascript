35da9dce0fd411c91ecccf550bb6eb79d81c4e25
osmium2579 <71938969+osmium2579@users.noreply.github.com>
2020-12-08 00:07:27 +0900

    savingUserName_part2_finished

--- makingClock/gretting.js
+++ makingClock/gretting.js
@@ -5,19 +5,35 @@ const form = document.querySelector(".js-form"),//querySelector는 원하는 셀
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
+function saveName(text){
+    localStorage.setItem(USER_LS,text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit)
+}
+
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-greeting.innerText = `Hello ${text}`;
+    greeting.innerText = `Hello ${text}`;
 }//paintGreeting은 하나의 argument(인자)값을 가짐.
 
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
-    if (currentUser === nell){
-        //she is not
+    if (currentUser === null){
+        askForName();
     }
     else{
-        paintGreeting(cureentUser);
+        paintGreeting(currentUser);
     }
 }
 
